bb612977224d4c8c93ae77459d9449f65dc742b4
bhargavabhiram1999 <bhargavabhiram1999@gmail.com>
2024-11-02 18:20:51 +0530

    adas

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -3,22 +3,12 @@ import axios from 'axios';
 const API_URL = 'https://konnect-1.onrender.com/api/users'; 
 
 const getUsers = async () => {
-    const response = await axios.get(API_URL, {
-        auth: {
-            username: 'admin', // Replace with your actual username
-            password: 'Bhargav@06', // Replace with your actual password
-        },
-    });
+    const response = await axios.get(API_URL);
     return response.data;
 };
 
 const createUser = async (userData) => {
-    const response = await axios.post(API_URL, userData, {
-        auth: {
-            username: 'admin', // Replace with your actual username
-            password: 'Bhargav@06', // Replace with your actual password
-        },
-    });
+    const response = await axios.post(API_URL, userData);
     return response.data;
 };
 
