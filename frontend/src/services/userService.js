3da22b5da5ab7f7c03a1172f26a112ea161d9164
bhargavabhiram1999 <bhargavabhiram1999@gmail.com>
2024-11-02 16:58:29 +0530

    dcs

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/users'; 
+const API_URL = 'https://konnect-1.onrender.com/api/users'; 
 
 const getUsers = async () => {
     const response = await axios.get(API_URL, {
